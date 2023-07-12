2b289ded6f73b7f371eb4b260ac28f8e74e12a06
Sanzhar09 <s.adambekov09@gmail.com>
2023-07-12 17:44:54 +0600

    add test react task

---
+++ react-projects/test/src/App.js
@@ -0,0 +1,80 @@
+import './index.scss';
+import {useState} from 'react';
+
+
+const questions = [
+  {
+    title: 'React - это ... ?',
+    variants: ['библиотека', 'фреймворк', 'приложение'],
+    correct: 0,
+  },
+  {
+    title: 'Компонент - это ... ',
+    variants: ['приложение', 'часть приложения или страницы', 'то, что я не знаю что такое'],
+    correct: 1,
+  },
+  {
+    title: 'Что такое JSX?',
+    variants: [
+      'Это простой HTML',
+      'Это функция',
+      'Это тот же HTML, но с возможностью выполнять JS-код',
+    ],
+    correct: 2,
+  },
+];
+
+function Result({ correct}) {
+  return (
+    <div className="result">
+      <img alt='result' src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
+      <h2>Вы отгадали {correct} ответа из {questions.length}</h2>
+      <a href='/'>
+      <button>Попробовать снова</button>
+      </a>
+
+    </div>
+  );
+}
+
+function Game( {step,question,onClickVariant}) {
+  const percentage = Math.round((step/questions.length) *100)
+
+  return (
+    <>
+      <div className="progress">
+        <div style={{ width:`${percentage}%` }} className="progress__inner"></div>
+      </div>
+      <h1>{question.title}</h1>
+      <ul>
+        <li>{question.variants.map((text,index)=> <li onClick= {()=>onClickVariant(index)} key={text}>{text}</li>)}</li>
+      </ul>
+    </>
+  );
+}
+
+function App() {
+  const [correct,setCorrect]= useState(0)
+  const[step, setStep]=useState(0)
+  const question = questions[step];
+  console.log(question)
+  const onClickVariant =(index)=> {
+    
+    if( index==question.correct) {
+      setCorrect(correct + 1)
+    }
+    setStep(step+1)
+
+  }
+  return (
+    <div className="App">
+      {
+        step!==questions.length?
+        <Game step={step} onClickVariant={onClickVariant} question={question} />
+      :<Result correct = {correct}/>
+      }
+    </div>
+  );
+}
+
+export default App;
